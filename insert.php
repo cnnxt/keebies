104b7158b08dfa58b0c1c7d3639c7cb34ab0e6aa
cnnxt <conniextu@gmail.com>
2024-05-28 21:00:55 -0700

    all initial files

---
+++ insert.php
@@ -0,0 +1,31 @@
+
+<?php
+
+// Get values from form
+$name=$_POST['name'];
+$email=$_POST['email'];
+$inquiry=$_POST['inquiry'];
+
+$to = "conniextu@gmail.com";
+$subject = "inquiry about keebies";
+$message = " name: " . $name . "\r\n email: " . $email . "\r\n inquiry: " . $inquiry;
+
+
+$from = "keebie website";
+$headers = "from:" . $from . "\r\n";
+$headers .= "Content-type: text/plain; charset=UTF-8" . "\r\n"; 
+
+if(@mail($to,$subject,$message,$headers))
+{$from="from: $name<$email>\r\nReturn-path: $email"; 
+        $subject="inquiry about keebies"; 
+        mail("conniextu@gmail.com", $subject, $message, $from); 
+        echo "thanks for sending in your inquiry!"; 
+ 
+  // Created by Future Tutorials
+}else{
+  echo "error! please try again.";
+}
+
+
+
+?>
